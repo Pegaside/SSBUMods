efc921eee7a1f5e346ce0d1b100c15bf3ab6eaf5
Pegaside <ocyomecniv@gmail.com>
2026-04-05 01:28:07 -0700

    Autosave 2026-04-05 01:28:07

--- joker nairu's love/src/joker/mod.rs
+++ joker nairu's love/src/joker/mod.rs
@@ -205,8 +205,8 @@ unsafe extern "C" fn zelda_specialn_status_pre(fighter: &mut L2CFighterCommon) -
         (*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_N
             | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK
             | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON) as u64,
-        *FIGHTER_STATUS_ATTR_START_TURN as u32,
-        *FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_N as u32,
+        FIGHTER_STATUS_ATTR_START_TURN.into(),
+        FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_N.into(),
         0,
     );
 
