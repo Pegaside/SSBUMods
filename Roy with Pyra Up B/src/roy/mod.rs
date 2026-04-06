00493861c186c1ef3e562eeec1467b1d87fb6bfe
Pegaside <ocyomecniv@gmail.com>
2026-04-06 09:58:07 -0700

    Autosave 2026-04-06 09:58:07

--- Roy with Pyra Up B/src/roy/mod.rs
+++ Roy with Pyra Up B/src/roy/mod.rs
@@ -125,7 +125,84 @@ unsafe extern "C" fn eflame_specialhi_substatus_end(fighter: &mut L2CFighterComm
 // SpecialHiEnd
 // ----------
 
+// STATUS Pre eflame_specialhiend_status_pre
+unsafe extern "C" fn eflame_specialhiend_status_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    StatusModule::init_settings(
+        fighter.module_accessor,
+        smash::app::SituationKind(*SITUATION_KIND_GROUND),
+        *FIGHTER_KINETIC_TYPE_GROUND_STOP,
+        GROUND_CORRECT_KIND_GROUND.into(),
+        smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE),
+        true,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_ALL_FLAG,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_ALL_INT,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_ALL_FLOAT,
+        0,
+    );
 
+    FighterStatusModuleImpl::set_fighter_status_data(
+        fighter.module_accessor,
+        false,
+        *FIGHTER_TREADED_KIND_NO_REAC,
+        false,
+        false,
+        false,
+        (*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_HI
+            | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK) as u64,
+        0,
+        *FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_HI,
+        0,
+    );
+
+    0.into()
+}
+
+
+// STATUS Main eflame_specialhiend_status_main
+unsafe extern "C" fn eflame_specialhiend_status_main(fighter: &mut L2CFighterCommon) -> L2CValue {
+    MotionModule::change_motion(
+        fighter.module_accessor,
+        Hash40::new("special_hi"),
+        0.0,
+        1.0,
+        false,
+        0.0,
+        false,
+        false,
+    );
+
+    KineticModule::clear_speed_all(fighter.module_accessor);
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_MOTION);
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_GRAVITY);
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_CONTROL);
+    KineticModule::unable_energy(fighter.module_accessor, *FIGHTER_KINETIC_ENERGY_ID_STOP);
+
+    fighter.sub_shift_status_main(L2CValue::Ptr(eflame_specialhiend_status_main_loop as *const () as _,
+    ))
+}
+
+
+// STATUS MainLoop eflame_specialhiend_status_main_loop
+unsafe extern "C" fn eflame_specialhiend_status_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if CancelModule::is_enable_cancel(fighter.module_accessor) {
+        if fighter.sub_wait_ground_check_common(false.into()).get_bool()
+            || fighter.sub_air_check_fall_common().get_bool()
+        {
+            return 0.into();
+        }
+    }
+
+    if fighter.global_table[0x16].get_i32() == *SITUATION_KIND_AIR {
+        fighter.change_status(FIGHTER_STATUS_KIND_FALL_SPECIAL.into(), false.into());
+    } else if MotionModule::is_end(fighter.module_accessor) {
+        fighter.change_status(FIGHTER_STATUS_KIND_WAIT.into(), false.into());
+    }
+
+    0.into()
+}
+
+
+// STATUS End eflame_specialhiend_status_end
 
 // ----------
 // SpecialHiJump
