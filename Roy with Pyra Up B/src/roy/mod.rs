18cf6d14e248516e01faef3b493464fb65aab9e7
Pegaside <ocyomecniv@gmail.com>
2026-04-06 08:28:07 -0700

    Autosave 2026-04-06 08:28:07

--- Roy with Pyra Up B/src/roy/mod.rs
+++ Roy with Pyra Up B/src/roy/mod.rs
@@ -10,28 +10,49 @@ use {
     smashline::{*, Priority::*}
 };
 
-// Game acmd script
+// Game ACMD Scripts
+// ACMD Game Ground
 unsafe extern "C" fn example_acmd_script(agent: &mut L2CAgentBase) {
     
 }
 
-// Char opff, Global opff
-unsafe extern "C" fn fighter_frame(fighter: &mut L2CFighterCommon) {
-    
-}
 
-// Status script
+// All Status Scripts
+
+// ----------
+// SpecialHi
+// ----------
+
+// STATUS Init
 unsafe extern "C" fn example_status_script(fighter: &mut L2CFighterCommon) -> L2CValue {
     0.into()
 }
 
+// ----------
+// SpecialHiEnd
+// ----------
+
+
+
+// ----------
+// SpecialHiJump
+// ----------
+
+
+
+// ----------
+// SpecialHiLoop
+// ----------
+
+
+
+// ----------
+// Fire Pillar SpecialHi
+// ----------
+
 pub fn install() {
     Agent::new("mario")
         .game_acmd("game_ATTACK_NAME_HERE", example_acmd_script, Default) // Game acmd script
-        .on_line(Main, fighter_frame) // Char opff
         .status(Main, *FIGHTER_MARIO_STATUS_KIND_SPECIAL_LW_CHARGE, example_status_script) // Status script
-        .install();
-    Agent::new("fighter")
-        .on_line(Main, fighter_frame) // Global opff
         .install();
 }
