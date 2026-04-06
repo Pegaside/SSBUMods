2e9d68bbaecb7cdabf8f456eea46f1ed2a95b64f
Pegaside <ocyomecniv@gmail.com>
2026-04-06 09:28:07 -0700

    Autosave 2026-04-06 09:28:07

--- Roy with Pyra Up B/src/roy/mod.rs
+++ Roy with Pyra Up B/src/roy/mod.rs
@@ -73,7 +73,7 @@ unsafe extern "C" fn eflame_specialhi_status_main(fighter: &mut L2CFighterCommon
 
 
 // STATUS MainLoop eflame_specialhi_status_main_loop
-unsafe extern "C" fn eflame_specialhi_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
+unsafe extern "C" fn eflame_specialhi_status_main_loop(fighter: &mut L2CFighterCommon) -> L2CValue {
     if !MotionModule::is_end(fighter.module_accessor) {
         fighter.sub_change_motion_by_situation(
             Hash40::new("special_hi_start").into(),
@@ -98,7 +98,14 @@ unsafe extern "C" fn eflame_specialhi_main_loop(fighter: &mut L2CFighterCommon) 
 
 
 // STATUS End eflame_specialhi_status_end
+unsafe extern "C" fn eflame_specialhi_status_end(fighter: &mut L2CFighterCommon) -> L2CValue {
+    if fighter.global_table[0xb].get_i32() != *FIGHTER_EFLAME_STATUS_KIND_SPECIAL_HI_JUMP {
+        eflame_specialhi_substatus_end(fighter); // TODO: confirm exact identity/body of FUN_71000108a0
+        // TODO: confirm whether you want to rename global_table[0xb] to a known table constant in your setup
+    }
 
+    0.into()
+}
 
 
 
