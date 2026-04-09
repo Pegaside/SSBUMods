e5f7f991ac85728c939e31c069082874b91cbf35
Pegaside <ocyomecniv@gmail.com>
2026-04-09 03:38:07 -0700

    Autosave 2026-04-09 03:38:07

--- Joker Explosive Flame/src/joker/mod.rs
+++ Joker Explosive Flame/src/joker/mod.rs
@@ -15,15 +15,60 @@ unsafe extern "C" fn example_acmd_script(agent: &mut L2CAgentBase) {
     
 }
 
-// Char opff, Global opff
-unsafe extern "C" fn fighter_frame(fighter: &mut L2CFighterCommon) {
-    
-}
 
-// Status script
-unsafe extern "C" fn example_status_script(fighter: &mut L2CFighterCommon) -> L2CValue {
+//--------------------
+// STATUS SCRIPTS
+//--------------------
+
+// STATUS Pre palutena_specials_status_pre
+unsafe extern "C" fn palutena_specials_status_pre(fighter: &mut L2CFighterCommon) -> L2CValue {
+    let is_smash_dash_input =
+        (fighter.global_table[0x22].get_i32() & *FIGHTER_PAD_CMD_CAT3_FLAG_SPECIAL_S_SMASH_DASH) != 0;
+
+    if is_smash_dash_input {
+        WorkModule::on_flag(
+            fighter.module_accessor,
+            *FIGHTER_PALUTENA_STATUS_SPECIAL_S_FLAG_SMASH,
+        );
+    } else {
+        WorkModule::off_flag(
+            fighter.module_accessor,
+            *FIGHTER_PALUTENA_STATUS_SPECIAL_S_FLAG_SMASH,
+        );
+    }
+
+    StatusModule::init_settings(
+        fighter.module_accessor,
+        smash::app::SituationKind(*SITUATION_KIND_NONE),
+        *FIGHTER_KINETIC_TYPE_UNIQ,
+        GROUND_CORRECT_KIND_KEEP.into(),
+        smash::app::GroundCliffCheckKind(*GROUND_CLIFF_CHECK_KIND_NONE),
+        true,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_PALUTENA_SPECIAL_S_FLAG,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_PALUTENA_SPECIAL_S_INT,
+        *FIGHTER_STATUS_WORK_KEEP_FLAG_PALUTENA_SPECIAL_S_FLOAT,
+        0,
+    );
+
+    FighterStatusModuleImpl::set_fighter_status_data(
+        fighter.module_accessor,
+        false,
+        *FIGHTER_TREADED_KIND_NO_REAC,
+        false,
+        false,
+        false,
+        (*FIGHTER_LOG_MASK_FLAG_ATTACK_KIND_SPECIAL_S
+            | *FIGHTER_LOG_MASK_FLAG_ACTION_CATEGORY_ATTACK
+            | *FIGHTER_LOG_MASK_FLAG_ACTION_TRIGGER_ON
+            | *FIGHTER_LOG_MASK_FLAG_SHOOT) as u64,
+        0,
+        *FIGHTER_POWER_UP_ATTACK_BIT_SPECIAL_N as u32,
+        0,
+    );
+
     0.into()
 }
+
 
 pub fn install() {
     Agent::new("mario")
